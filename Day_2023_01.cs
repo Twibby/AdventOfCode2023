4c678c7a34bfb05c826758b0cfc1073f79367e84
ThibaudTGD <thibaud.ollivier@thegooddrive.net>
2023-11-17 11:54:17 +0100

    Init 2023 template script

---
+++ Day_2023_01.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Day_2023_01 : DayScript2023
+{
+    protected override string part_1()
+    {
+        foreach (string instruction in _input.Split('\n'))
+        {
+
+        }
+
+        return base.part_1();
+    }
+
+    protected override string part_2()
+    {
+        foreach (string instruction in _input.Split('\n'))
+        {
+
+        }
+        
+        return base.part_2();
+    }
+}
